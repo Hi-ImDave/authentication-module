515accf629b4514513d29f6f78f20a1e8a705ff0
David Mott <d.s.mott0808@gmail.com>
2023-01-13 16:09:28 -0800

    updated welcome

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -9,12 +9,11 @@ const Welcome = () => {
     <div className='hero min-h-screen bg-emerald-500 bg-opacity-60'>
       <div className='hero-content text-center text-neutral-content'>
         <div className='max-w-md'>
-          <h1 className='mb-5 text-5xl font-bold'>Chat Module</h1>
+          <h1 className='mb-5 text-5xl font-bold'>Authentication Module</h1>
           <p className='mb-5'>
-            Welcome. This is a modular chat application with full authentication
+            Welcome. This is a modular application with full authentication
             features. The purpose of this application is to integrate it with
-            web applications that require a chat feature. While it is fully
-            capable, it is not intended to perform as a stand-alone application.
+            web applications that require user authentication.
           </p>
         </div>
       </div>
